eed0f5a9fa6dc6052492fbde016db74676e6acb4
Erik Vullings <erik.vullings@gmail.com>
2016-05-23 06:59:12 +0200

    Removed old file-based REST service

--- rest/typings/browser.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="browser/ambient/body-parser/index.d.ts" />
-/// <reference path="browser/ambient/express-jwt/index.d.ts" />
-/// <reference path="browser/ambient/express-serve-static-core/index.d.ts" />
-/// <reference path="browser/ambient/express/index.d.ts" />
-/// <reference path="browser/ambient/jsonwebtoken/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/serve-static/index.d.ts" />
