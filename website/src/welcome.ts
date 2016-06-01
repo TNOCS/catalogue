ca4d033d28ee1a4c8af9845c08798046d51af1d9
Erik Vullings <erik.vullings@gmail.com>
2016-06-01 07:23:13 +0200

    TNO favicon added

--- website/src/welcome.ts
+++ website/src/welcome.ts
@@ -44,8 +44,8 @@ export class Welcome {
   }
 }
 
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
-  }
-}
+// export class UpperValueConverter {
+//   toView(value) {
+//     return value && value.toUpperCase();
+//   }
+// }
