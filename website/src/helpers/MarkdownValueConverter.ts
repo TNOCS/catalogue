3ccdb64647aa4f7f0840227a8457dc90891a0ecd
Erik Vullings <erik.vullings@gmail.com>
2016-06-08 18:02:19 +0200

    Markdown: when there is no input, output a dash

--- website/src/helpers/MarkdownValueConverter.ts
+++ website/src/helpers/MarkdownValueConverter.ts
@@ -2,7 +2,8 @@ import * as md from 'marked';
 
 export class MarkdownValueConverter {
     toView(str: string) {
-        return str && md(str, {
+        if (!str) return '-';
+        return md(str, {
             renderer:    new md.Renderer(),
             gfm:         true,
             tables:      true,
