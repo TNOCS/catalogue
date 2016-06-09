39226404762a36905fae5727981aa953fe39c872
Erik Vullings <erik.vullings@gmail.com>
2016-06-09 07:59:06 +0200

    Simplified password generator
    So you can more easily copy the password using a double click.

--- website/src/helpers/Utils.ts
+++ website/src/helpers/Utils.ts
@@ -1,6 +1,6 @@
 export class Utils {
     static generatePassword(length = 8) {
-        var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_!#$",
+        var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
             retVal = "";
         for (var i = 0, n = charset.length; i < length; ++i) {
             retVal += charset.charAt(Math.floor(Math.random() * n));
